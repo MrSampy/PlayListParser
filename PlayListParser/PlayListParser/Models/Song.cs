4b21bdeee07b41291c04942d4cd33d2ebae6fe02
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-09-21 22:51:21 +0300

    Add Song model

---
+++ PlayListParser/PlayListParser/Models/Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayListParser.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Duration { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj is Song other)
+            {
+                return Title == other.Title && Artist == other.Artist && Duration == other.Duration;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Title, Artist, Duration);
+        }
+
+    }
+}
