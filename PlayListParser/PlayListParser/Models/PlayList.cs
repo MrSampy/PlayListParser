c830f9b0065c49fd094e61d9ac16e9ba33a3fef0
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-09-21 22:51:29 +0300

    Add PlayList model

---
+++ PlayListParser/PlayListParser/Models/PlayList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayListParser.Models
+{
+    public class PlayList
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public List<Song> Songs { get; set; }
+
+        public PlayList()
+        {
+            Songs = new List<Song>();
+        }
+
+    }
+}
