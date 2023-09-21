04053f1b97caf9059c8c0c1c0ec33c7be86bd731
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-09-21 22:58:35 +0300

    Refactor Program.cs

--- PlayListParser/PlayListParser/Program.cs
+++ PlayListParser/PlayListParser/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PlayListParser;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        string url_1 = "https://music.amazon.com/playlists/B08BWK8W15";
+        string url_2= "https://music.amazon.com/albums/B001230JXC";
+
+        PlayListScraper scraper = new PlayListScraper();
+
+        var playlist = scraper.ScrapPlayListFromUrl(url_1);
+    }
+}
